d7e81807eaa21df7012e1b47c1c809ed59b5a484
LeVente <berrylevente@gmail.com>
2025-03-21 12:15:43 -0400

    adding drinks sql table definition

--- schema.sql
+++ schema.sql
@@ -14,3 +14,21 @@ CREATE TABLE users (
 INSERT INTO users (first_name, last_name, email, password_hash, role)
 VALUES ('John', 'Doe', 'johndoe@example.com', 'password', 'admin'),
        ('Jane', 'Doe', 'janedoe@example.com', 'password', 'customer')
+
+DROP TABLE IF EXISTS drinks;
+CREATE TABLE drinks (
+    drink_id SERIAL PRIMARY KEY,
+    name VARCHAR(100) NOT NULL,
+    description TEXT,
+    price DECIMAL(5, 2) NOT NULL,
+    image_url VARCHAR(255),
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+INSERT INTO drinks (name, description, price, image_url)
+VALUES ('Margarita', 'A classic cocktail made with tequila, lime juice, and triple sec.', 8.00, 'https://www.thecocktaildb.com/images/media/drink/5noda61589575158.jpg'),
+       ('Old Fashioned', 'A cocktail made by muddling sugar with bitters, then adding alcohol, such as whiskey or brandy, and a twist of citrus rind.', 10.00, 'https://www.thecocktaildb.com/images/media/drink/vrwquq1478252802.jpg'),
+       ('Martini', 'A cocktail made with gin and vermouth, and garnished with an olive or a lemon twist.', 12.00, 'https://www.thecocktaildb.com/images/media/drink/71t8581504353095.jpg'),
+       ('Mojito', 'A cocktail made with rum, sugar, lime juice, soda water, and mint.', 9.00, 'https://www.thecocktaildb.com/images/media/drink/3z6xdi1589574603.jpg'),
+       ('Cosmopolitan', 'A cocktail made with vodka, triple sec, cranberry juice, and freshly squeezed lime juice.', 10.00, 'https://www.thecocktaildb.com/images/media/drink/upxxpq1439907580.jpg');
