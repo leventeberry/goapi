51ff90555280314206e81925de33a45f3d63a2de
LeVente <berrylevente@gmail.com>
2025-04-19 17:36:10 -0400

    dev udpates

--- main.go
+++ main.go
@@ -1,12 +1,12 @@
 package main
 
 import(
-	// "fmt"
+	"fmt"
     "os"
 	"github.com/gin-gonic/gin"
     "github.com/leventeberry/goapi/initializers"
-	// "github.com/leventeberry/gomysql"
-	// "github.com/leventeberry/goapi/controllers"
+	"github.com/leventeberry/gomysql"
+	"github.com/leventeberry/goapi/controllers"
 )
 
 func init() {
@@ -16,11 +16,11 @@ func init() {
 
 func main() {
     // Create a connection pool to the database
-    // db, err := gomysql.ConnectDB("tcp", "localhost:3306", "testapi")
-    // if err != nil {
-    //     fmt.Println("Database connection error:", err)
-    //     return
-    // }
+    db, err := gomysql.ConnectDB("tcp", "localhost:3306", "testapi")
+    if err != nil {
+        fmt.Println("Database connection error:", err)
+        return
+    }
 
     // Create a new router
     router := gin.Default()
@@ -33,15 +33,15 @@ func main() {
         })
     })
 
-    // router.POST("/login", controllers.LoginUser(db))
-    // router.POST("/register", controllers.SignupUser(db))
+    router.POST("/login", controllers.LoginUser(db))
+    router.POST("/register", controllers.SignupUser(db))
 
-    // // User Routes
-    // router.GET("/users", controllers.GetUsers(db))
-    // router.GET("/users/:id", controllers.GetUser(db))
-    // router.POST("/users", controllers.CreateUser(db))
-    // router.PUT("/users/:id", controllers.UpdateUser(db))
-    // router.DELETE("/users/:id", controllers.DeleteUser(db))
+    // User Routes
+    router.GET("/users", controllers.GetUsers(db))
+    router.GET("/users/:id", controllers.GetUser(db))
+    router.POST("/users", controllers.CreateUser(db))
+    router.PUT("/users/:id", controllers.UpdateUser(db))
+    router.DELETE("/users/:id", controllers.DeleteUser(db))
 
     // Start the server (blocking call)
     router.Run(os.Getenv("PORT"))
