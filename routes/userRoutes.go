13179623f09284ff8dcfcbdeb5a676fe6c6d914d
hokage-216 <berrylevente@gmail.com>
2026-01-01 10:55:12 -0500

    User routes are now protected with authentication middleware

--- routes/userRoutes.go
+++ routes/userRoutes.go
@@ -1,21 +1,24 @@
 package routes
 
 import (
-    "github.com/gin-gonic/gin"
-    "github.com/leventeberry/goapi/controllers"
-    "gorm.io/gorm"
+	"github.com/gin-gonic/gin"
+	"github.com/leventeberry/goapi/controllers"
+	"github.com/leventeberry/goapi/middleware"
+	"gorm.io/gorm"
 )
 
-// SetupRoutes registers all application routes on the provided Gin engine.
+// SetupUserRoutes registers all user-related routes on the provided Gin engine.
+// All user routes are protected by authentication middleware.
 func SetupUserRoutes(router *gin.Engine, db *gorm.DB) {
 
-    // User routes group
-    userGroup := router.Group("/users")
-    {
-        userGroup.GET("", controllers.GetUsers(db))
-        userGroup.GET("/:id", controllers.GetUser(db))
-        userGroup.POST("", controllers.CreateUser(db))
-        userGroup.PUT("/:id", controllers.UpdateUser(db))
-        userGroup.DELETE("/:id", controllers.DeleteUser(db))
-    }
+	// User routes group with authentication middleware
+	userGroup := router.Group("/users")
+	userGroup.Use(middleware.AuthMiddleware())
+	{
+		userGroup.GET("", controllers.GetUsers(db))
+		userGroup.GET("/:id", controllers.GetUser(db))
+		userGroup.POST("", controllers.CreateUser(db))
+		userGroup.PUT("/:id", controllers.UpdateUser(db))
+		userGroup.DELETE("/:id", controllers.DeleteUser(db))
+	}
 }
